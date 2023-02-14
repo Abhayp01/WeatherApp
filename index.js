f7c251b56e27c287365782548f7cccdd1ea2fb93
Abhayp01 <prajapatiabhay2003@gmail.com>
2023-02-14 20:09:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const https = require("https");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("views"));
+
+var city = "Delhi";
+var weatherData;
+var temp;
+var temp1;
+var temp2;
+var temp3;
+var temp4;
+var weatherDescription
+var icon;
+var icon1;
+var icon2;
+var icon3;
+var icon4;
+var imgUrl;
+var imgUrl1;
+var imgUrl2;
+var imgUrl3;
+var imgUrl4;
+var weekdays=["Mon","Tue","Wed","Thurs","Friday","Sat","Sun"];
+var today=new Date();
+var a=today.getDay();
+
+app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=c69fde2bab13cfa24384be7da34bc2fc&units=metric";
+    https.get(url, function (require) {
+        require.on("data", function (data) {
+            weatherData = JSON.parse(data);
+            temp = weatherData.list[0].main.temp;
+            temp1 = weatherData.list[8].main.temp;
+            temp2 = weatherData.list[16].main.temp;
+            temp3 = weatherData.list[24].main.temp;
+            temp4 = weatherData.list[32].main.temp;
+            icon1 = weatherData.list[8].weather[0].icon;
+            icon2 = weatherData.list[16].weather[0].icon;
+            icon3 = weatherData.list[24].weather[0].icon;
+            icon4 = weatherData.list[32].weather[0].icon;
+            weatherDescription = weatherData.list[0].weather[0].description;
+            icon = weatherData.list[0].weather[0].icon;
+            imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            imgUrl1 = "http://openweathermap.org/img/wn/" + icon1 + "@2x.png";
+            imgUrl2 = "http://openweathermap.org/img/wn/" + icon2 + "@2x.png";
+            imgUrl3 = "http://openweathermap.org/img/wn/" + icon3 + "@2x.png";
+            imgUrl4 = "http://openweathermap.org/img/wn/" + icon4 + "@2x.png";
+            
+            
+            res.render("weather", {
+                mycity: city,
+                day1: weekdays[a%weekdays.length],
+                day2: weekdays[(a+1)%weekdays.length],
+                day3: weekdays[(a+2)%weekdays.length],
+                day4: weekdays[(a+3)%weekdays.length],
+                day5: weekdays[(a+4)%weekdays.length],
+                temp: temp,
+                temp1: temp1,
+                temp2: temp2,
+                temp3: temp3,
+                temp4: temp4,
+                weatherDescription: weatherDescription,
+                imgUrl: imgUrl,
+                imgUrl1: imgUrl1,
+                imgUrl2: imgUrl2,
+                imgUrl3: imgUrl3,
+                imgUrl4: imgUrl4
+            });
+        })
+    })
+});
+
+
+app.post("/", function (req, res) {
+    city = req.body.city;
+
+
+    // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c69fde2bab13cfa24384be7da34bc2fc&units=metric";
+    // https.get(url, function (response) {
+    //     response.on("data", function (data) {
+    //         weatherData = JSON.parse(data);
+    //         temp = weatherData.main.temp;
+    //         weatherDescription = weatherData.weather[0].description;
+    //         icon = weatherData.weather[0].icon;
+            
+    //         imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    //         res.redirect("/")
+
+    //     })
+    // })
+    const url = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=c69fde2bab13cfa24384be7da34bc2fc&units=metric";
+    https.get(url, function (response) {
+        response.on("data", function (data) {
+            weatherData = JSON.parse(data);
+            temp = weatherData.list[0].main.temp;
+            temp1 = weatherData.list[8].main.temp;
+            temp2 = weatherData.list[16].main.temp;
+            temp3 = weatherData.list[24].main.temp;
+            temp4 = weatherData.list[32].main.temp;
+            weatherDescription = weatherData.list[0].weather[0].description;
+            icon = weatherData.list[0].weather[0].icon;
+            icon1 = weatherData.list[8].weather[0].icon;
+            icon2 = weatherData.list[16].weather[0].icon;
+            icon3 = weatherData.list[24].weather[0].icon;
+            icon4 = weatherData.list[32].weather[0].icon;
+            
+            imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            imgUrl1 = "http://openweathermap.org/img/wn/" + icon1 + "@2x.png";
+            imgUrl2 = "http://openweathermap.org/img/wn/" + icon2 + "@2x.png";
+            imgUrl3 = "http://openweathermap.org/img/wn/" + icon3 + "@2x.png";
+            imgUrl4 = "http://openweathermap.org/img/wn/" + icon4 + "@2x.png";
+            
+            
+            res.redirect("/")
+
+        })
+    })
+
+
+
+
+});
+
+
+
+
+
+app.listen(process.env.PORT||4000, function () {
+    console.log("server started at 4000");
+
+});
